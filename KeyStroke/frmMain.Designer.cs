13b8fd28721cd44d17682c5a0807e5a5528bfd99
Afshin Rahimi <arahimi@uwindsor.ca>
2021-09-08 15:50:25 -0400

    Add project files.

---
+++ KeyStroke/frmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace KeyStroke
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.ctx1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ni1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ctx1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ctx1
+            // 
+            this.ctx1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.ctx1.Name = "ctx1";
+            this.ctx1.Size = new System.Drawing.Size(104, 54);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // ni1
+            // 
+            this.ni1.ContextMenuStrip = this.ctx1;
+            this.ni1.Icon = ((System.Drawing.Icon)(resources.GetObject("ni1.Icon")));
+            this.ni1.Text = "KeyStroke";
+            this.ni1.Visible = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KeyStroke.Properties.Resources.keyboard;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 108);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Stroke";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Developed by Afshin Rahimi ";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 192);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KeyStroke";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ctx1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip ctx1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon ni1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ KeyStroke/frmMain.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyStroke
+{
+    public partial class frmMain : Form
+    {
+        Stopwatch clock = new Stopwatch();
+        frmPopup lastfrm = null;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private GlobalKeyboardHook _globalKeyboardHook;
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            // EDT: No need to filter for VkSnapshot anymore. This now gets handled
+            // through the constructor of GlobalKeyboardHook(...).
+            //if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.SysKeyDown &&
+            //    e.KeyboardData.Flags == GlobalKeyboardHook.LlkhfAltdown)
+            //{
+            //    MessageBox.Show("Alt + Print Screen");
+            //    e.Handled = true;
+            //}
+
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown | e.KeyboardState == GlobalKeyboardHook.KeyboardState.SysKeyDown)
+            {
+                // Now you can access both, the key and virtual code
+                Keys loggedKey = e.KeyboardData.Key;
+                int loggedVkCode = e.KeyboardData.VirtualCode;
+                Debug.WriteLine(loggedKey + " | " + loggedVkCode + " | ");
+
+                string txt = loggedKey.ToString();
+                if (e.KeyboardData.Flags == GlobalKeyboardHook.LlkhfAltdown | loggedKey.ToString().ToLower() == "rmenu" | loggedKey.ToString().ToLower() == "lmenu")
+                {
+                    txt = "Alt";
+                }
+                if (loggedVkCode >= 65 & loggedVkCode <= 90)
+                {
+                    char c = (char)loggedVkCode;
+                    txt = c.ToString();
+                } else
+                {   //http://www.911fonts.com/font/download_KeystrokesMTRegular_5831.htm
+                    if (loggedKey.ToString().ToLower()=="lshiftkey" | loggedKey.ToString().ToLower() == "rshiftkey")
+                    {
+                        txt = "Shift" ;
+                    }
+                    if (loggedKey.ToString().ToLower() == "lcontrolkey" | loggedKey.ToString().ToLower() == "rcontrolkey")
+                    {
+                        txt = "CTRL";
+                    }
+                    if (loggedKey.ToString().ToLower() == "lwin")
+                    {
+                        txt = "❖";
+                    }
+                    if (loggedKey.ToString().ToLower() == "return")
+                    {
+                        txt = "Enter";
+                    }
+                    if (loggedKey.ToString().ToLower() == "space")
+                    {
+                        txt = " ";
+                    }
+                    if (loggedKey.ToString().ToLower() == "escape")
+                    {
+                        txt = "Esc";
+                    }
+                    if (loggedKey.ToString().ToLower() == "capital")
+                    {
+                        txt = "Caps Lock";
+                    }
+                    if (loggedKey.ToString().ToLower() == "left")
+                    {
+                        txt = "←";
+                    }
+                    if (loggedKey.ToString().ToLower() == "right")
+                    {
+                        txt = "→";
+                    }
+                    if (loggedKey.ToString().ToLower() == "up")
+                    {
+                        txt = "↑";
+                    }
+                    if (loggedKey.ToString().ToLower() == "down")
+                    {
+                        txt = "↓";
+                    }
+                    if (loggedKey.ToString().Length==2 & loggedKey.ToString().StartsWith("D"))
+                    {
+                        txt = loggedKey.ToString().Substring(1);
+                    }
+                    if (loggedKey.ToString().StartsWith("Oem"))
+                    {
+                        txt = loggedKey.ToString().Replace("Oem", "");
+                    }
+
+                }
+
+                if (!clock.IsRunning)
+                {
+                    clock.Start();
+                }
+                double elapsed = clock.Elapsed.TotalMilliseconds;
+
+                if (elapsed>1000 | lastfrm == null)
+                {
+                    clock.Restart();
+                    frmPopup frm = new frmPopup();
+                    lastfrm = frm;
+                    Label lbl = (Label)frm.Controls.Find("lblKeys", true).FirstOrDefault();
+                    lbl.Text = txt;
+                    frm.Show();
+                    frm.Left = 0;
+                    frm.Height = lbl.Height;
+                    frm.Top = Screen.PrimaryScreen.Bounds.Height - frm.Height - 100;
+                    frm.Width = lbl.Width;
+                } else
+                {
+                    clock.Restart();
+                    frmPopup frm = lastfrm;
+                    Label lbl = (Label)frm.Controls.Find("lblKeys", true).FirstOrDefault();
+                    if (lbl == null)
+                    {
+                        return;
+                    }
+                    bool combined = false;
+                    if (lbl.Text.ToLower().StartsWith("shift") | lbl.Text.ToLower().StartsWith("ctrl") | lbl.Text.ToLower().StartsWith("❖") | lbl.Text.ToLower().StartsWith("alt"))
+                        {
+                        combined = true;
+                    }
+                    lbl.Text = lbl.Text + (combined? " + ":"") + txt;
+                    frm.Show();
+                    frm.Left = 0;
+                    frm.Height = lbl.Height;
+                    frm.Top = Screen.PrimaryScreen.Bounds.Height - frm.Height - 100;
+                    frm.Width = lbl.Width;
+                }
+
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Hooks only into specified Keys (here "A" and "B").
+            //_globalKeyboardHook = new GlobalKeyboardHook(new Keys[] { Keys.A, Keys.B });
+
+            // Hooks into all keys.
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+            this.Visible = false;
+            ni1.Visible = true;
+            this.ShowInTaskbar = false;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
